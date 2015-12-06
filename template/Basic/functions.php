3807c553b32ff3d991ba2e3c44203584f293d6a7
Saiful Islam <codersaiful@gmail.com>
2015-12-06 18:40:53 +0600

    Create functions.php
    Update from office pc.
    and any customization possible on template and html,css design as well as in query

---
+++ template/Basic/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * This is function file to add any new function for your site. just need to add functions.php file
+ * nowhere need to include
+ */
+
+function randomize_color_value(){
+    //
+}
