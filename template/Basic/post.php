000060e304d3f2756f119e693e3fbbcaabc5b4c3
Saiful Islam <codersaiful@gmail.com>
2015-12-06 18:42:52 +0600

    Update post.php
    Update from office pc.
    and any customization possible on template and html,css design as well as in query

--- template/Basic/post.php
+++ template/Basic/post.php
@@ -1,1 +1,17 @@
-
+<?php get_header(); ?>
+<div class="column clear main_content">
+<?php
+$sql = "SELECT * FROM post WHERE slug = '{$slug}'  LIMIT 1";
+$result = $conn->query($sql);
+if($row = $result->fetch_assoc()){
+?>
+<div class="post_wraper">
+    <h1 title="<?php echo $row['title']; ?>"><?php echo $row['title']; ?></h1><hr>
+    Posted: <?php echo $row['time']; ?><br>
+    <p><?php echo $row['content'] ?></p>
+</div>
+<?php
+}
+?>
+</div>
+<?php get_footer(); ?>
