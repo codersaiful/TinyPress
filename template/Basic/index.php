7d73b1b5f0224770cb7c41eb0572382c1e72134e
Saiful Islam <codersaiful@gmail.com>
2015-12-06 18:41:32 +0600

    Update index.php
    Update from office pc.
    and any customization possible on template and html,css design as well as in query

--- template/Basic/index.php
+++ template/Basic/index.php
@@ -1,1 +1,16 @@
-
+<?php get_header();?>
+<?php
+$post_sql = "SELECT * FROM post WHERE type = 'page' AND ( id = '44' OR id = '47') ORDER BY id DESC"; 
+$post_result = $conn->query($post_sql);
+while($post_row = $post_result->fetch_assoc()):
+?>
+<div class="column clear main_content">
+        <h3 title="<?php echo $post_row['title'] ?>"><?php echo $post_row['title'] ?></h3>
+        <?php echo $post_row['content'] ?>
+</div>
+<?php
+endwhile;
+?>
+<?php get_comment(); ?>
+<?php get_footer(); ?>
+<h1><?php my_comment(); ?></h1>
