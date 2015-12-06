bd89c18abb0d53bc2233b1c017d554d8ddae0234
Saiful Islam <codersaiful@gmail.com>
2015-12-06 18:42:14 +0600

    Update page.php
    Update from office pc.
    and any customization possible on template and html,css design as well as in query

--- template/Basic/page.php
+++ template/Basic/page.php
@@ -1,1 +1,20 @@
+<?php get_header(); ?>
+<?php
 
+
+
+$sql = "SELECT * FROM post WHERE slug = '{$slug}'  LIMIT 1";
+$result = $conn->query($sql);
+if($row = $result->fetch_assoc()){
+?>
+<div class="column clear main_content">
+    <h3><?php echo $row['title']; ?></h3>
+    Posted: <?php echo $row['time']; ?><br>
+    <p><?php echo $row['content'] ?></p>
+</div>
+<?php
+}
+
+?>
+<?php get_comment(); ?>
+<?php get_footer(); ?>
