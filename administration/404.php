61cca283ed8983ffd29e6da7d10e5c7207f563b6
Saiful Islam <codersaiful@gmail.com>
2015-12-06 19:06:06 +0600

    Update 404.php
    office

--- administration/404.php
+++ administration/404.php
@@ -1,1 +1,4 @@
-
+<div style="border: 1px solid #eaeaea; padding: 30px;margin:15px auto;max-width: 650px;width: 100%;min-width: 250px;">
+<h2 style="color: #d00;text-align: center;">404</h2>
+<p  style="color: gray;text-align: center; margin: 15px auto;"><i style="color:green;padding:0 10px;"><?php echo $dharsboar_url; ?></i>requested file not found</p>
+</div>
