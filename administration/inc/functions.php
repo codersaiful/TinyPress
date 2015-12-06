829c7d4fbf80a97c1664d469b0925c8e46ddc90f
Saiful Islam <codersaiful@gmail.com>
2015-12-06 19:14:52 +0600

    Update functions.php

--- administration/inc/functions.php
+++ administration/inc/functions.php
@@ -1,1 +1,18 @@
-functions.php
+<?php
+
+/* 
+These function only for Admin use
+ */
+function admin_css($css_file_name = null){
+    if(!isset($css_file_name)):
+    $avail_able_css_folder = scandir(CSS,TRUE);
+    foreach ($avail_able_css_folder as $each_css){
+        if(strlen($each_css)>2){
+            $css_file = CSS_URL.$each_css;
+            echo '<link rel="stylesheet" type="text/css" href="'.$css_file.'">';
+        }
+    }
+    else:
+        echo '<link rel="stylesheet" type="text/css" href="'.CSS_URL.$css_file_name.'.css">';
+    endif;
+}
