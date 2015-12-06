f9a92225a3d3c3b80cf9e88632c188eae3952689
Saiful Islam <codersaiful@gmail.com>
2015-12-06 19:11:52 +0600

    Update new.php

--- administration/new.php
+++ administration/new.php
@@ -1,1 +1,9 @@
-new.php
+<?php
+if(isset($_GET['type']) && $_GET['type'] == TRUE){
+    $type = $_GET['type'];
+}
+include ADMIN . 'inc/new.php';
+
+/*
+ * if want to change new page/post form, go to inc folder than edit new.php file
+ */
