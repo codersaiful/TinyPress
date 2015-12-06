b6c1b412595ccaca6f929d636795df2676ada781
Saiful Islam <codersaiful@gmail.com>
2015-12-06 18:46:58 +0600

    Update sidebar.php
    from office

--- template/Basic/sidebar.php
+++ template/Basic/sidebar.php
@@ -1,1 +1,14 @@
-sidebar file
+<?php
+/*
+*This page is suitable for sidebar section of your site.
+*you can also add for footer or any other section.
+*/
+?>
+<h4>Sidebar title</h4>
+<something>
+    <title_tag>Sidebar widget's Title</title_tag>
+    <other_thing>
+        Widget's conetnt. 
+    </other_thing>
+</something>
+
