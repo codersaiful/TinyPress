edde41a599a7fbc2ed3926d4ee81454fe6f3b8a0
Saiful Islam <codersaiful@gmail.com>
2015-12-06 18:38:20 +0600

    Update footer.php
    Update from office pc.
    and any customization possible on template and html,css design as well as in query

--- template/Basic/footer.php
+++ template/Basic/footer.php
@@ -1,1 +1,23 @@
-
+			<br class="clear">
+                        <!--
+			<div class="column clear menu center">
+                            <?php menu('main_menu',array(
+                                'container_tag'     =>  'nav',
+                                'container_class'   =>  'my_footer_menu_custom',
+                                'class'             =>  'my_menu_ul_class_test',
+                                'id'                =>  'slslslskd',
+                                'container_id'      =>  'kslslskd'
+                            )); ?>
+			</div>
+                        -->
+			<br class="clear">
+			<div class="column clear center footer">
+				
+				<p class="center">
+                                    All Right reserve by <a href="https://elance.com/s/codersaiful" target="_blank"><b><?php site_info(); ?></b></a>
+				</p>
+			</div>			
+		<br class="clear">	
+		</div>
+	</body>
+</html>
