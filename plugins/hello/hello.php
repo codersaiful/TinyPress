dc0da3cbcde04ee1c1c9b74feaa219696e83def9
Saiful Islam <codersaiful@gmail.com>
2015-12-06 18:56:39 +0600

    Create hello.php
    from office

---
+++ plugins/hello/hello.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * This is a same and test plugin.
+ * actually if u want to add plugin,
+ * just make a directory in plugins direction than make same name file .php
+ */
+
+function hello(){
+    echo "<h1>This is hello</h1>";
+}
+function my_comment(){
+    echo 'Nothing from plugin.';
+}
