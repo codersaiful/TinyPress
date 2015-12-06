6e419ae18cbee1e899245192492358bfeab1ccec
Saiful Islam <codersaiful@gmail.com>
2015-12-06 19:15:27 +0600

    Update msg.php

--- administration/inc/msg.php
+++ administration/inc/msg.php
@@ -1,1 +1,13 @@
-msg.php
+<?php
+
+if(isset($msg) || isset($_GET['msg'])){
+    if(isset($_GET['msg'])){
+        $msg[] = $_GET['msg'];
+    }
+
+    echo '<ul style="color: green;" class="error_list">';
+    foreach ($msg as $message){
+        echo '<li>'.$message.'</li>';
+    }
+    echo '</ul>';
+}
