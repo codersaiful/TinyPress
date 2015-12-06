c4dda0945d7d4336162353b92d10a39865fa1b54
Saiful Islam <codersaiful@gmail.com>
2015-12-06 18:38:56 +0600

    Update header.php
    Update from office pc.
    and any customization possible on template and html,css design as well as in query

--- template/Basic/header.php
+++ template/Basic/header.php
@@ -1,1 +1,11 @@
 
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php title(); ?></title>
+		<link rel="stylesheet" type="text/css" media="screen" href="<?php echo TEMPLATE_URL; ?>css/style.css">
+                <link rel="stylesheet" type="text/css" media="screen" href="<?php echo TEMPLATE_URL; ?>css/fonts/style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<div class="wrapper">
