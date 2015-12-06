71e9691da7ec938fc6f5664a20ff450954e111c4
Saiful Islam <codersaiful@gmail.com>
2015-12-06 18:35:19 +0600

    Update 404.php
    update from office pc.
    And any design/update/html edit possible

--- template/Basic/404.php
+++ template/Basic/404.php
@@ -1,1 +1,8 @@
-Default 404 error page for site of selected template.
+<?php get_header('sitemap'); ?>
+<div class="column clear main_content">
+    <h1 title="<?php title(); ?>" style="text-align: center;color: #d00;font-size: 300%;"><?php title(); ?></h1>
+        <p style="text-align: center;color: brown;">We are strongly sorry that, Your desired page is not found.
+            Please try with correct URL. Go to <a href="<?php echo SITE_URL; ?>">Home Page</a>
+        </p>
+</div>
+<?php get_footer(); ?>
