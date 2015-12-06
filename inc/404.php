159e6078f8b57b8283e83a0c502cad30fd507d33
Saiful Islam <codersaiful@gmail.com>
2015-12-06 18:59:54 +0600

    Update 404.php
    added from office

--- inc/404.php
+++ inc/404.php
@@ -1,1 +1,9 @@
-
+<html>
+    <head>
+        <title>404 ERROR - NOT FOUND</title>
+    </head>
+    <body>
+        <h1 style="text-align: center;color: #d00;">404</h1>
+        <p style="text-align: center;color: #ddd;">Default 404 page.</p>
+    </body>
+</html>
