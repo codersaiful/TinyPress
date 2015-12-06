3d16b26a18f45c84fd2a2453e0b761e7f8a322af
Saiful Islam <codersaiful@gmail.com>
2015-12-06 19:12:19 +0600

    Update post.php

--- administration/post.php
+++ administration/post.php
@@ -1,1 +1,11 @@
+<?php
+$type = 'post';
+if(isset($_GET['type']) && $_GET['type'] == TRUE){
+    $type = $_GET['type'];
+}
+include ADMIN . 'inc/list.php';
 
+
+/*
+ * if want to change all post/page page go to inc folder than edit list.php file
+ */
