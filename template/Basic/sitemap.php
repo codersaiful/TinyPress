a3f705fb8c55cf00f7e92176af564d2fe3a10a42
Saiful Islam <codersaiful@gmail.com>
2015-12-06 18:48:11 +0600

    Create sitemap.php
    from office

---
+++ template/Basic/sitemap.php
@@ -0,0 +1,20 @@
+<?php get_header('sitemap'); ?>
+<?php
+
+
+
+$sql = "SELECT * FROM post WHERE slug = '{$slug}'  LIMIT 1";
+$result = $conn->query($sql);
+if($row = $result->fetch_assoc()){
+?>
+<div class="column clear main_content">
+    <h3><?php echo $row['title']; ?></h3>
+    Posted: <?php echo $row['time']; ?><br>
+    <p><?php echo $row['content'] ?></p>
+</div>
+<?php
+}
+
+?>
+<?php get_comment(); ?>
+<?php get_footer(); ?>
