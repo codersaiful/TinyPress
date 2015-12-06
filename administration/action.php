1880337b381d092df3c980ea26c12c0a4af5df89
Saiful Islam <codersaiful@gmail.com>
2015-12-06 19:06:49 +0600

    Update action.php
    office

--- administration/action.php
+++ administration/action.php
@@ -1,1 +1,16 @@
-
+<?php
+if(isset($_GET['status']) && $_GET['status'] == 'delete' && isset($_GET['type']) && isset($_GET['id']) && isset($_GET['slug'])){
+    //$status = $_GET['status'];
+    $type = $_GET['type'];
+    $id = $_GET['id'];
+    $slug = $_GET['slug'];
+        $sql = "DELETE FROM post WHERE id = '{$id}'";
+        $sql_2 = "DELETE FROM controller WHERE slug = '{$slug}'";
+        if($conn->query($sql) == TRUE && $conn->query($sql_2) == TRUE){
+            header("Location: post.php?type=".$type."&msg=SucceffullyDeleted");
+        }else{
+          header("Location: post.php?type=".$type."&err=ProblemInPerform");  
+        }
+    
+    
+}
